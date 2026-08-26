8d81c1d5cca93918c0443f92259190f992df21b9
MatiCHad <matiashernandezd21@gmail.com>
2026-08-26 10:47:58 -0300

    Engine versión 2

--- src/Library/Engine.cs
+++ src/Library/Engine.cs
@@ -7,10 +7,10 @@ namespace Library
 {
     public class Engine
     {
-        public Cell[,] NextGeneration(Cell[,] tablero)
+        public Cell[,] NextGeneration(Board tablero)
         {
-            int ancho = tablero.GetLength(Board.Ancho);
-            int altura = tablero.GetLength(Board.Alto);
+            int ancho = tablero.Ancho;
+            int altura = tablero.Alto;
             Cell[,] tableroClon = new Cell[ancho, altura];
 
             for (int x = 0; x < ancho; x++)
@@ -22,35 +22,34 @@ namespace Library
                     {
                         for (int j = y - 1; j <= y + 1; j++)
                         {
-                            if (i >= 0 && i < ancho && j >= 0 && j < altura && tablero[i, j].GetEstado())
+                            if (i >= 0 && i < ancho && j >= 0 && j < altura && tablero.ObtenerCelula(i, j).GetEstado())
                             {
                                 vecinosVivos++;
                             }
                         }
                     }
-
-                    if (tablero[x, y].GetEstado())
+                    if (tablero.ObtenerCelula(x, y).GetEstado())
                     {
                         vecinosVivos--;
                     }
 
                     Cell nuevaCelula = new Cell();
 
-                    if (tablero[x, y].GetEstado() && vecinosVivos < 2)
+                    if (tablero.ObtenerCelula(x, y).GetEstado() && vecinosVivos < 2)
                     {
                         nuevaCelula.SetEstado(false); // muere por baja población
                     }
-                    else if (tablero[x, y].GetEstado() && vecinosVivos > 3)
+                    else if (tablero.ObtenerCelula(x, y).GetEstado() && vecinosVivos > 3)
                     {
                         nuevaCelula.SetEstado(false); // muere por sobrepoblación
                     }
-                    else if (!tablero[x, y].GetEstado() && vecinosVivos == 3)
+                    else if (!tablero.ObtenerCelula(x, y).GetEstado() && vecinosVivos == 3)
                     {
                         nuevaCelula.SetEstado(true); // nace por reproducción
                     }
                     else
                     {
-                        nuevaCelula.SetEstado(tablero[x, y].GetEstado()); // mantiene el estado
+                        nuevaCelula.SetEstado(tablero.ObtenerCelula(x, y).GetEstado()); // mantiene el estado
                     }
 
                     tableroClon[x, y] = nuevaCelula;
